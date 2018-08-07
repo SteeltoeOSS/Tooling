5d379d51149671283fcde4f8bdb2358d8483296f
Chris Cheetham <chris@cheetham.com>
2018-08-07 19:33:48 -0400

    Scripts for running unit and feature tests on Windows

---
+++ scripts/feature-test.ps1
@@ -0,0 +1,18 @@
+$pwd = Get-Location
+Set-Location $PSScriptRoot\..\feature
+
+[int]$errors = 0
+
+Get-ChildItem -Directory -Filter "*.Feature" | ForEach-Object {
+	Set-Location $_.Name
+	dotnet test
+	$errors = $errors + $lastexitcode
+	Set-Location ..
+}
+
+Set-Location $pwd
+
+if ($errors -gt 0)
+{
+	Throw "$errors feature test(s) failed"
+}
---
+++ scripts/unit-test.ps1
@@ -0,0 +1,18 @@
+$pwd = Get-Location
+Set-Location $PSScriptRoot\..\test
+
+[int]$errors = 0
+
+Get-ChildItem -Directory -Filter "*.Test"| ForEach-Object {
+	Set-Location $_.Name
+	dotnet test
+	$errors = $errors + $lastexitcode
+	Set-Location ..
+}
+
+Set-Location $pwd
+
+if ($errors -gt 0)
+{
+	Throw "$errors unit test(s) failed"
+}
