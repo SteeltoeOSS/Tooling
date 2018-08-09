eedc3532b68328ca728b79db6b9dcbdede209fc0
Chris Cheetham <chris@cheetham.com>
2018-08-09 17:33:04 -0400

    Provides a logging toehold

---
+++ src/Steeltoe.Tooling.DotnetCli.Base/Logging.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Steeltoe.Tooling.DotnetCli.Base
+{
+    public static class Logging
+    {
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory().AddConsole(LogLevel.Debug);
+    }
+}
