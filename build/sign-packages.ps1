536e1931a7ec684c75efe6becd136b700bfbddc8
Chris Cheetham <chris@cheetham.com>
2019-10-10 14:38:11 -0500

    Signs packages prior to publishing
    [#63]

---
+++ build/sign-packages.ps1
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+
+$baseDir = "$PSScriptRoot/.."
+$toolDir = "$baseDir/tools"
+$appSettings = "$PSScriptRoot/appsettings.json"
+if ($null -eq $ArtifactDirectory) {
+    $ArtifactDirectory = "."
+}
+
+if ([string]::IsNullOrEmpty($Env:SignClientSecret)) {
+    Write-Host "SignClientSecret not set, exiting"
+    Exit 1
+}
+
+New-Item -ItemType Directory -Force -Path $toolDir
+dotnet tool install --tool-path $toolDir signclient
+
+$nupkgs = Get-ChildItem $ArtifactDirectory/Steeltoe*.*nupkg -recurse | Select-Object -ExpandProperty FullName
+foreach ($nupkg in $nupkgs) {
+    Write-Host "signing $nupkg"
+    & $toolDir/SignClient 'sign' -c $appSettings -i $nupkg -r $Env:SignClientUser -s $Env:SignClientSecret -n 'Steeltoe' -d 'Steeltoe' -u 'https://github.com/SteeltoeOSS'
+}
