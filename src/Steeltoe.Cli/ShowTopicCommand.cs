178d8b60aec78f904eb6ae79ee87dbaf396adf08
Chris Cheetham <chris@cheetham.com>
2020-02-17 11:12:13 -0500

    impl show-topic command

--- src/Steeltoe.Cli/ShowTopicCommand.cs
+++ src/Steeltoe.Cli/ShowTopicCommand.cs
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Tooling;
 using Steeltoe.Tooling.Controllers;
 
 namespace Steeltoe.Cli
@@ -32,7 +36,7 @@ Examples:
         public const string CommandName = "show-topic";
 
         [Argument(0, Name = "topic", Description = "Topic")]
-        private string Topic { get; set; }
+        private string Topic { get; } = null;
 
         public ShowTopicCommand(IConsole console) : base(console)
         {
@@ -40,7 +44,66 @@ Examples:
 
         protected override Controller GetController()
         {
-            throw new NotImplementedException();
+            return new ShowTopicController(Topic);
+        }
+
+        private class ShowTopicController : Controller
+        {
+            private static readonly ILogger<ShowTopicController> Logger =
+                Logging.LoggerFactory.CreateLogger<ShowTopicController>();
+
+            private readonly string _topicsPath = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "steeltoe.rc",
+                "topics");
+
+            private readonly string _topic;
+
+            public ShowTopicController(string topic)
+            {
+                _topic = topic;
+            }
+
+            protected override void Execute()
+            {
+                Logger.LogDebug($"topics path: {_topicsPath}");
+                if (_topic == null)
+                {
+                    ListTopics();
+                }
+                else
+                {
+                    ShowTopic(_topic);
+                }
+            }
+
+            private void ListTopics()
+            {
+                var topicPaths = Directory.GetFiles(_topicsPath, "*.txt");
+                var maxTopicLength = topicPaths.Select(Path.GetFileNameWithoutExtension)
+                    .Select(topic => topic.Length).Concat(new[] {0}).Max();
+                var descriptionColumn = maxTopicLength + 4;
+                foreach (var topicPath in topicPaths)
+                {
+                    var topic = Path.GetFileNameWithoutExtension(topicPath);
+                    var description = File.ReadLines(topicPath).First();
+                    Context.Console.WriteLine($"{topic.PadRight(descriptionColumn)}{description}");
+                }
+            }
+
+            private void ShowTopic(string topic)
+            {
+                var topicPath = Path.Combine(_topicsPath, $"{topic}.txt");
+                if (!File.Exists(topicPath))
+                {
+                    throw new ItemDoesNotExistException(topic);
+                }
+
+                foreach (var line in File.ReadLines(topicPath))
+                {
+                    Context.Console.WriteLine(line);
+                }
+            }
         }
     }
 }
