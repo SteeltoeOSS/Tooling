5501e6ab32d3825defaf8aefe16bcbb283384679
Chris Cheetham <chris@cheetham.com>
2020-03-26 11:12:33 -0400

    Allow test to pass regardless of framework

--- test/Steeltoe.Cli.Test/ShowFeature.cs
+++ test/Steeltoe.Cli.Test/ShowFeature.cs
@@ -62,7 +62,7 @@ namespace Steeltoe.Cli.Test
                     "project:",
                     "name: show",
                     "file: show.csproj",
-                    "framework: netcoreapp3.1",
+                    "*",
                     "image: mcr.microsoft.com/dotnet/core/sdk:3.1",
                     "protocols:",
                     "- name: http",
